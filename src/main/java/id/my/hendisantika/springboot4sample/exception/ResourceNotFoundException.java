083bdef915a3f15d9630f9fde98f91a71870c33b
hendisantika <hendisantika@yahoo.co.id>
2025-12-15 13:59:50 +0700

    feat: add ResourceNotFoundException🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springboot4sample/exception/ResourceNotFoundException.java
@@ -0,0 +1,27 @@
+package id.my.hendisantika.springboot4sample.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot4-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 15/12/25
+ * Time: 09.00
+ * To change this template use File | Settings | File Templates.
+ */
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
+        super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
+    }
+}
