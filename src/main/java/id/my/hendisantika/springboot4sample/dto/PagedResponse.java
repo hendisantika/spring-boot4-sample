04dd47917ab4cb46833ce793cb1c7c53be511a35
hendisantika <hendisantika@yahoo.co.id>
2025-12-15 13:59:58 +0700

    feat: add PagedResponse🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springboot4sample/dto/PagedResponse.java
@@ -0,0 +1,34 @@
+package id.my.hendisantika.springboot4sample.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot4-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 15/12/25
+ * Time: 09.00
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PagedResponse<T> {
+
+    private List<T> content;
+    private int page;
+    private int size;
+    private long totalElements;
+    private int totalPages;
+    private boolean last;
+    private boolean first;
+}
