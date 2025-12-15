a99651e6420f5d5c607f6d6df237020cc3cd3216
hendisantika <hendisantika@yahoo.co.id>
2025-12-15 13:59:29 +0700

    feat: add Product Response🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springboot4sample/dto/ProductResponse.java
@@ -0,0 +1,37 @@
+package id.my.hendisantika.springboot4sample.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot4-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 15/12/25
+ * Time: 09.00
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductResponse {
+
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Integer quantity;
+    private String category;
+    private Boolean isActive;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
